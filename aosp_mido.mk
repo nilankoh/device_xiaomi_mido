c7005a7331444ced1ee4e6e852ace8c6ad0d5e79
root <root@instance-2.c.aexhalmod.internal>
2018-05-16 13:45:11 +0000

    AEX-fy

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -20,7 +20,7 @@ $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 TARGET_ARCH := arm64
 TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 PRODUCT_NAME := aosp_mido
 BOARD_VENDOR := Xiaomi
